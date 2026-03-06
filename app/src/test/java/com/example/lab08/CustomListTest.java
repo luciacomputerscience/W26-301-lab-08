b2253a4ca95f344ebdfb2e0b5db1b62a49ce2e91
luciacomputerscience <teamnamenoname@gmail.com>
2026-03-06 14:38:49 -0700

    Pushing failing test testDeleteCity() (deleteCity(city) does not exist yet)

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -12,4 +12,19 @@ public class CustomListTest {
         //This will fail initially because hasCity() DNE
         assertTrue(list.hasCity(calgary));
     }
+
+    @Test void testDeleteCity() {
+        // Create two new dummy cities and add them to the list
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        City edmonton = new City("Edmonton", "AB");
+        list.addCity(calgary);
+        list.addCity(edmonton);
+
+        // Delete only ONE of the cities and check the other is still there
+        // Test will fail initially because deleteCity(city) DNE
+        list.deleteCity(calgary);
+        assertFalse(list.hasCity(calgary));
+        assertTrue(list.hasCity(edmonton));
+    }
 }
